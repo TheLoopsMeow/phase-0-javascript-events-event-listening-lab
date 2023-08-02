c2346ada3b887acb4e2f000ca57d84ecad04829c
Wyatt <wyattstephenson@rocketmail.com>
2023-08-02 13:49:49 -0600

    0 JAVASCRIPT EVENTS EVENTLISTENERS

--- index.js
+++ index.js
@@ -9,6 +9,11 @@ function addingEventListener () {
   input.addEventListener('click', clickAlert);
 }
 
+const input = document.getElementById('button');
+input.addEventListener('click', function() {
+  alert('I was clicked!');
+});
+
 
 
 
